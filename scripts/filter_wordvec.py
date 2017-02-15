d1806d095e56fcf1a197fce0c2d89832375da231
Yijia Liu <oneplus.lau@gmail.com>
2017-02-14 20:31:19 -0800

    add filter_wordvec

---
+++ scripts/filter_wordvec.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+vocab = set()
+for f in sys.argv[2:]:
+	for line in open(f, 'r'):
+		words = line.strip().split()[1:]
+		for word in words:
+			vocab.add(word)
+
+for line in open(sys.argv[1], 'r'):
+	word = line.strip().split()[0]
+	if word in vocab:
+		print line.strip()
