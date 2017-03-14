e73d008f3cfc3151c727bc93c36d590fe3a42b6e
Oneplus <oneplus.lau@gmail.com>
2017-03-14 18:24:26 -0400

    fix

--- scripts/yelp/extract_label_and_document.py
+++ scripts/yelp/extract_label_and_document.py
@@ -13,22 +13,25 @@ fpo_lab = open(sys.argv[1] + '.lab', 'w')
 fpo_doc = open(sys.argv[1] + '.doc', 'w')
 
 for line in codecs.open(sys.argv[1], 'r', encoding='utf-8'):
-	# type(line) => 'unicode'
-	label, raw_document = line.strip().split(',', 1)
-	raw_document = raw_document[1:-1]
-	raw_document = raw_document.replace(u'\\""', u'\\"')
-	raw_document = raw_document.decode('unicode_escape')
-	raw_paragraphs = raw_document.split(u'\n')
-	for raw_paragraph in raw_paragraphs:
-		if len(raw_paragraph) == 0:
-			continue
-		raw_sentences = sentence_tokenizer.tokenize(raw_paragraph)
-		for raw_sentence in raw_sentences:
-			if len(raw_sentence) == 0:
-				continue
-			assert u'\n' not in raw_sentence, 'document: {0}'.format(did)
-			print(raw_sentence.encode('utf-8'), file=fpo_doc)
-	did += 1
-	print(file=fpo_doc)
-	label = int(label[1:-1]) - 1
-	print(label, file=fpo_lab)
+    # type(line) => 'unicode'
+    label, raw_document = line.strip().split(',', 1)
+    raw_document = raw_document[1:-1]
+    raw_document = raw_document.strip()
+    raw_document = raw_document.replace(u'\\""', u'\\"')
+    raw_document = raw_document.decode('unicode_escape')
+    raw_document = raw_document.replace(u'\r', u'\n')
+    raw_paragraphs = raw_document.split(u'\n')
+    for raw_paragraph in raw_paragraphs:
+        if len(raw_paragraph) == 0:
+            continue
+        raw_paragraph = raw_paragraph.strip()
+        raw_sentences = sentence_tokenizer.tokenize(raw_paragraph)
+        for raw_sentence in raw_sentences:
+            if len(raw_sentence) == 0:
+                continue
+            assert u'\n' not in raw_sentence, 'document: {0}'.format(did)
+            print(raw_sentence.encode('utf-8'), file=fpo_doc)
+    did += 1
+    print(file=fpo_doc)
+    label = int(label[1:-1]) - 1
+    print(label, file=fpo_lab)
--- scripts/yelp/lower_and_merge.py
+++ scripts/yelp/lower_and_merge.py
@@ -7,19 +7,21 @@ label_fpi = open(sys.argv[1], 'r')
 document_fpi = open(sys.argv[2], 'r')
 
 for line in document_fpi:
-	if len(line.strip()) == 0:
-		assert label_fpi
-		label = label_fpi.readline()
-		print(label.strip())
-		data = data.lower().replace('-lrb-', '(').replace('-rrb-', ')')
-		print(data)
-		data = ''
-	else:
-		data += line
+    if len(line.strip()) == 0:
+        label = label_fpi.readline()
+        assert len(label) > 0
+        print(label.strip())
+        data = data.lower().replace('-lrb-', '(').replace('-rrb-', ')')
+        if len(data.strip()) == 0:
+            data = ''
+            continue
+        print(data)
+        data = ''
+    else:
+        data += line
 
 if len(data) > 0:
-	assert label_fpi
-	label = label_fpi.readline()
-	print(label.strip())
-	print(data.strip().lower())
-	print()
+    label = label_fpi.readline()
+    assert len(label) > 0
+    print(label.strip())
+    print(data.strip().lower())
