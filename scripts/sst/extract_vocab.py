047a9e51d676f720bcfacbc5936037ec6dbe246c
Yijia Liu <oneplus.lau@gmail.com>
2017-03-22 16:17:56 -0700

    add

--- scripts/sst/extract_vocab.py
+++ scripts/sst/extract_vocab.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
+import argparse
 
-freq = int(sys.argv[2])
-word_count = {}
-for line in open(sys.argv[1], 'r'):
-    tokens = line.strip().split()
-    for word in tokens[1:]:
-        if word not in word_count:
-            word_count[word] = 0
-        word_count[word] += 1
 
-for key in word_count:
-    if word_count[key] > freq:
-        print(key)
+def main():
+    cmd = argparse.ArgumentParser('')
+    cmd.add_argument('--freq', default=0, type=int, required=True, help='the minimal frequency.')
+    cmd.add_argument('--lowercase', default=False, action='store_true', help='lowercase')
+    cmd.add_argument('files', nargs='*', help='the path to the files')
+    args = cmd.parse_args()
+    word_count = {}
+
+    for f in args.files:
+        for line in open(f, 'r'):
+            tokens = line.strip().split()
+            for word in tokens[1:]:
+                if args.lowercase:
+                    word = word.lower()
+                if word not in word_count:
+                    word_count[word] = 0
+                word_count[word] += 1
+
+    for key, freq in sorted(word_count.items()):
+        if freq > args.freq:
+            print(key)
+
+if __name__ == "__main__":
+    main()
