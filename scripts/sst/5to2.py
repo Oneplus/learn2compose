6d122a163d16e5ff3a9e6ee0ce908d4c7cbfe0d8
Oneplus <oneplus.lau@gmail.com>
2017-04-19 10:06:56 -0700

    fix

--- scripts/sst/5to2.py
+++ scripts/sst/5to2.py
@@ -2,12 +2,26 @@
 # convert fine grain classification to binary classification problem
 # neutral is removed.
 import sys
+
+def convert(token):
+    if token == '2':
+        return 1
+    elif token in ('3', '4'):
+        return 2
+    else:
+        return 0
+
 for line in open(sys.argv[1], 'r'):
-	label, sentence = line.strip().split('\t')
-	if label == '2':
-		continue
-	elif label in ('3', '4'):
-		label = '1'
-	else:
-		label = '0'
-	print '{0}\t{1}'.format(label, sentence)
+    label, rest = line.strip().split('\t', 1)
+    if '\t' in rest:
+        sentence, rest = rest.split('\t', 1)
+        tokens = rest.split()
+        rest = ' '.join(['{0}:{1}'.format(token.split(':')[0], convert(token.split(':')[1])) for token in tokens])
+        rest = '{0}\t{1}'.format(sentence, rest)
+    if label == '2':
+        continue
+    elif label in ('3', '4'):
+        label = '2'
+    else:
+        label = '0'
+    print '{0}\t{1}'.format(label, rest)
--- src/misc.cc
+++ src/misc.cc
@@ -1,4 +1,8 @@
 #include "misc.h"
+#ifndef _MSC_VER
+# include <sys/types.h>
+# include <unistd.h>
+#endif
 
 int portable_getpid() {
 #ifdef _MSC_VER  
